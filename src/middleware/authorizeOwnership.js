60e12d30d8fa2214972f318389cd3cf2bd5a26e7
Juan Vargas <jvarga22@charlotte.edu>
2025-11-18 15:53:59 -0500

    Fix authorizeOwnership

--- src/middleware/authorizeOwnership.js
+++ src/middleware/authorizeOwnership.js
@@ -1,10 +1,10 @@
-import { getPostById } from "../services/postService.js";
+import { getProductById } from "../services/productService.js";
 
 export async function authorizeOwnership(req, res, next) {
-    const postId = parseInt(req.params.id);
-    const post = await getPostById(postId);
+    const productId = parseInt(req.params.id);
+    const product = await getPostById(postId);
 
-    if (post.userId !== req.user.id) {
+    if (product.userId !== req.user.id) {
         const error = new Error('Forbidden: insufficient permission');
         error.status = 403;
         return next(error);
