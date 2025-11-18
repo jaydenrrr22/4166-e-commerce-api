a7062c5a6e55b0c3a701610d381cd31e688fdd84
jaydenrrr22 <sryu@charlotte.edu>
2025-11-17 20:22:47 -0500

    added all functions in userRepo

--- src/user_repo/userRepo.js
+++ src/user_repo/userRepo.js
@@ -12,3 +12,49 @@ export async function getAllUsers() {
   });
   return users;
 }
+
+export async function getUserById(id) {
+  const users = await prisma.user.findUnique({
+    where: { id },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+      createdAt: true,
+    },
+  });
+  return users;
+}
+
+export async function createUser(user) {
+  const newUser = await prisma.user.create({
+    data: user,
+  });
+  return newUser;
+}
+
+export async function updateUser(id, user) {
+  try {
+    const updatedUser = await prisma.user.update({
+      where: { id },
+      data: user,
+    });
+    return updatedUser;
+  } catch (error) {
+    if (error.code === 'P2025') return null;
+    throw error;
+  }
+}
+
+export async function removeUser(id) {
+  try {
+    const deletedUser = await prisma.user.delete({
+      where: { id },
+    });
+    return deletedUser;
+  } catch (error) {
+    if (error.code === 'P2025') return null;
+    throw error;
+  }
+}
