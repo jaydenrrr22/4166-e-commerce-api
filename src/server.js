be29ed9643ff647fcbab6b375c7a85325988a008
gLombar4 <lombard.garrett@gmail.com>
2025-11-22 19:16:51 -0500

    adjust server.js file

--- src/server.js
+++ src/server.js
@@ -1,28 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-import swaggerUI from "swagger-ui-express";
-import YAML from "yamljs";
 
 import userRoutes from './routes/userRoutes.js';
 import authRoutes from './routes/authRoutes.js';
-import productRoutes from "./routes/productRoutes.js";
-
-import productRoutes from "./routes/productRoutes.js";
+import categoryRoutes from './routes/categoryRoutes.js';
+import productRoutes from './routes/productRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
-app.use(morgan('tiny'));
 
-const specs = YAML.load("./public/bundled.yaml");
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+app.use(morgan('tiny'));
 
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
-app.use('api/products', productRoutes);
+app.use('/api/categories', categoryRoutes);
+app.use('/api/products', productRoutes);
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
