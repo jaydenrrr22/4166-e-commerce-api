f001f9b425c61585a6b280b3d436d45ea2a61cc7
gabejimenez22 <gabejimenez22@outlook.com>
2025-11-23 19:31:03 -0500

    removed auth ownership

--- src/middleware/requireAdmin.js
+++
@@ -1,7 +0,0 @@
-
-export function requireAdmin(req, res, next) {
-    if (!req.user || req.user.role !== 'admin') {
-        return res.status(403).json({ error: "User not allowed to view other users’ orders." });
-    }
-    next();
-}
